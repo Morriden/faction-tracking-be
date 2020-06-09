679c03590df0ad4fb23a8f3dc2a3a8e1b194d07c
Erik Ford <ekford18@gmail.com>
2020-06-09 14:57:17 -0700

    user tests completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -117,3 +117,96 @@ describe('Faction routes', () => {
       });
   });
 });
+
+describe('Faction routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  it('creates a user', () => {
+    return request(app)
+      .post('/api/v1/adventurers')
+      .send({
+        name: 'Morriden',
+        class: 'Wizard',
+        image: 'image.url',
+        level: 8,
+        health: 47,
+        wealth: 39
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Morriden',
+          class: 'Wizard',
+          image: 'image.url',
+          level: 8,
+          health: 47,
+          wealth: 39,
+          __v: 0
+        });
+      });
+  });
+
+  it('It will update a adventurer by id', () => {
+    return Adventurer.create({
+      name: 'Morriden',
+      class: 'Wizard',
+      image: 'image.url',
+      level: 8,
+      health: 47,
+      wealth: 39
+    })
+      .then(adventurer => {
+        return request(app)
+          .patch(`/api/v1/adventurers/${adventurer._id}`)
+          .send({ name: 'Kartanis', class: 'Paladin', image: 'new image' });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Kartanis',
+          class: 'Paladin',
+          image: 'new image',
+          level: 8,
+          health: 47,
+          wealth: 39,
+          __v: 0
+        });
+      });
+  });
+
+  it('It will delete a adventurer with id', () => {
+    return Adventurer.create({
+      name: 'Morriden',
+      class: 'Wizard',
+      image: 'image.url',
+      level: 8,
+      health: 47,
+      wealth: 39
+    })
+      .then(adventurer => request(app).delete(`/api/v1/adventurers/${adventurer._id}`))
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Morriden',
+          class: 'Wizard',
+          image: 'image.url',
+          level: 8,
+          health: 47,
+          wealth: 39,
+          __v: 0
+        });
+      });
+  });
+});
