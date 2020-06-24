c7c0660920feb8d5ca852d0c9b80628d2bad6bec
Erik Ford <ekford18@gmail.com>
2020-06-24 14:26:13 -0700

    completed first test suite, data correct

--- __tests__/adventurer.test.js
+++ __tests__/adventurer.test.js
@@ -1,64 +1,16 @@
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongod = new MongoMemoryServer();
-const mongoose = require('mongoose');
-const connect = require('../lib/utils/connect');
-require('dotenv').config();
+const { agent } = require('../lib/data-seed/data-helper');
 
 const request = require('supertest');
 const app = require('../lib/app');
 const Adventurer = require('../lib/models/adventurer');
-const Faction = require('../lib/models/faction');
 const User = require('../lib/models/user');
 
+
 describe('Adventurer routes', () => {
-  beforeAll(async() => {
-    const uri = await mongod.getUri();
-    return connect(uri);
-  });
   
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  let newUser;
-  let newAdventurer;
-  let newFaction;
-  const agent = request.agent(app);
-
-  beforeEach(async() => {
-    newUser = await User.create({
-      email: 'test@test.com',
-      password: 'password'
-    });
-    newAdventurer = await Adventurer.create({
-      user: newUser.id,
-      name: 'Morriden',
-      class: 'Wizard',
-      image: 'image.url',
-      level: 8,
-      health: 47,
-      wealth: 39
-    });
-    newFaction = await Faction.create({
-      name: 'The Harpers',
-      description: 'description for the harpers organization.',
-      image: 'image.url.harpers.com'
-    });
-  
-    return agent 
-      .post('/api/v1/users/login')
-      .send({
-        email: 'test@test.com',
-        password: 'password'
-      });
-  });
-
-  afterAll(async() => {
-    await mongoose.connection.close();
-    return mongod.stop();
-  });
-  
-  it('creates a adventurer', () => {
+  it('creates a adventurer', async() => {
+    const newUser = await User.findOne();
+    
     return agent
       .post('/api/v1/adventurers')
       .send({
@@ -85,26 +37,30 @@ describe('Adventurer routes', () => {
       });
   });
 
-  it('get a adventurer by id', () => {
+  it('get a adventurer by id', async() => {
+    const newAdventurer = await Adventurer.findOne();
+
     return agent
       .get(`/api/v1/adventurers/${newAdventurer._id}`)
       .then(res => {
         expect(res.body).toEqual({
           factions: [],
           _id: expect.anything(),
-          name: 'Morriden',
-          class: 'Wizard',
-          image: 'image.url',
-          level: 8,
-          health: 47,
-          wealth: 39,
-          user: newUser.id,
+          name: newAdventurer.name,
+          class: newAdventurer.class,
+          image: newAdventurer.image,
+          level: newAdventurer.level,
+          health: newAdventurer.health,
+          wealth: newAdventurer.wealth,
+          user: newAdventurer.user.toString(),
           __v: 0
         });
       });
   });
   
-  it('It will update a adventurer by id', () => {
+  it('It will update a adventurer by id', async() => {
+    const newAdventurer = await Adventurer.findOne();
+
     return agent
       .patch(`/api/v1/adventurers/${newAdventurer._id}`)
       .send({ name: 'Kartanis', class: 'Paladin', image: 'new image' })
@@ -114,28 +70,30 @@ describe('Adventurer routes', () => {
           name: 'Kartanis',
           class: 'Paladin',
           image: 'new image',
-          level: 8,
-          health: 47,
-          wealth: 39,
-          user: newUser.id,
+          level: newAdventurer.level,
+          health: newAdventurer.health,
+          wealth: newAdventurer.wealth,
+          user: newAdventurer.user.toString(),
           __v: 0
         });
       });
   });
   
-  it('It will delete a adventurer with id', () => {
+  it('It will delete a adventurer with id', async() => {
+    const newAdventurer = await Adventurer.findOne();
+
     return agent
       .delete(`/api/v1/adventurers/${newAdventurer._id}`)
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
-          name: 'Morriden',
-          class: 'Wizard',
-          image: 'image.url',
-          level: 8,
-          health: 47,
-          wealth: 39,
-          user: newUser.id,
+          name: newAdventurer.name,
+          class: newAdventurer.class,
+          image: newAdventurer.image,
+          level: newAdventurer.level,
+          health: newAdventurer.health,
+          wealth: newAdventurer.wealth,
+          user: newAdventurer.user.toString(),
           __v: 0
         });
       });
